602669ee1e1f01397aa1ee077e372ad39c116afa
Miguel Pelaez <mdpl11@gmail.com>
2016-11-28 23:42:25 -0500

    Configuración inicial de la solución Web.Api con servicios Rest.

---
+++ Tns.Aerolinea.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tns.Aerolinea.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
