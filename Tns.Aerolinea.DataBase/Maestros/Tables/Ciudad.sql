a308a4f95f3e67a5903c68b23bd3c939c6d250fe
Miguel Peláez <mdpl11@gmail.com>
2016-12-02 00:25:13 -0500

    actualizacion proyecto bd

---
+++ Tns.Aerolinea.DataBase/Maestros/Tables/Ciudad.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Maestros].[Ciudad] (
+    [IdCiudad]      SMALLINT     IDENTITY (1, 1) NOT NULL,
+    [NombreCiudad]  VARCHAR (50) NOT NULL,
+    [FechaCreacion] DATETIME     NOT NULL,
+    [Activa]        BIT          CONSTRAINT [DF__Ciudad__Activa] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_Ciudad] PRIMARY KEY CLUSTERED ([IdCiudad] ASC),
+    CONSTRAINT [UK_Ciudad] UNIQUE NONCLUSTERED ([NombreCiudad] ASC)
+);
+
--- Tns.Aerolinea.DataBase/Security/Maestros.sql
+++ Tns.Aerolinea.DataBase/Security/Maestros.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Maestros]
     AUTHORIZATION [dbo];
 
+
+
