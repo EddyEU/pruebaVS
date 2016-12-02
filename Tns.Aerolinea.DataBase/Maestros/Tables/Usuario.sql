709ef03de3c83a71d07ab05fd3c0c831defdb2a2
Miguel Peláez <mdpl11@gmail.com>
2016-12-01 19:22:16 -0500

    Proyecto de base de datos.

---
+++ Tns.Aerolinea.DataBase/Maestros/Tables/Usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Maestros].[Usuario] (
+    [IdUsuario]     INT           IDENTITY (1, 1) NOT NULL,
+    [Login]         VARCHAR (100) NOT NULL,
+    [Clave]         VARCHAR (100) NOT NULL,
+    [FechaCreacion] DATETIME      CONSTRAINT [DF_Usuario_FechaCreacion] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED ([IdUsuario] ASC)
+);
+
---
+++ Tns.Aerolinea.DataBase/Security/Maestros.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [Maestros]
+    AUTHORIZATION [dbo];
+
